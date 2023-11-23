e7b78f0483e0b1bb137e6eef284fbe0ea973bff9
website-krysta <website.krysta@gmail.com>
2023-11-23 12:55:07 +0530

    adding allowed host

--- igniteap/igniteap/settings.py
+++ igniteap/igniteap/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-pqdtjqb)i#p+av5#4%uz6er_zot8^c)ad3=0#=4hw7#de492sx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
